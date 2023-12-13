41f54fb713cbbc45615bbf7123447e8b8641ef7e
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2023-12-13 15:44:15 -0500

    Refactor day 13

--- 13.ml
+++ 13.ml
@@ -2,51 +2,54 @@ let rec read_grids ic list accum =
   try
     let line = input_line ic in
     let list, accum = if line = String.empty then
-      ([], accum @ [Array.of_list list])
+      ([], (Array.of_list list) :: accum)
     else
       (list @ [Array.of_seq (String.to_seq line)], accum) in
     read_grids ic list accum
   with End_of_file ->
-    accum @ [Array.of_list list]
+    (Array.of_list list) :: accum
 
-let count_difference l1 l2 =
-  Array.fold_left (+) 0 (Array.map2 (fun a b -> if a = b then 0 else 1) l1 l2)
+let rec count_difference l1 l2 i accum =
+  if i = Array.length l1 then
+    accum
+  else if l1.(i) = l2.(i) then
+    count_difference l1 l2 (succ i) accum
+  else
+    count_difference l1 l2 (succ i) (succ accum)
 
-let test_horizontal_symmetry grid =
-  (* Find a row that is duplicated *)
-  let result = ref None in
-  for i = 0 to Array.length grid - 2 do
-    if grid.(i) = grid.(i + 1) then
-      let l = ref (i - 1) in
-      let r = ref (i + 2) in
-      let is_symmetric = ref true in
-      while !l >= 0 && !r < Array.length grid do
-        if not (grid.(!l) = grid.(!r)) then
-          is_symmetric := false;
-        l := !l - 1;
-        r := !r + 1;
-      done;
-      if !is_symmetric then
-        result := Some (i + 1)
-  done;
-  !result
+let rec test_exact_symmetry grid l r =
+  if l < 0 || r >= Array.length grid then
+    true
+  else if not (grid.(l) = grid.(r)) then
+    false
+  else
+    test_exact_symmetry grid (pred l) (succ r)
 
-let test_smudged_horizontal_symmetry grid =
-  (* Find a row that is duplicated *)
-  let result = ref None in
-  for i = 0 to Array.length grid - 2 do
-    let l = ref (i) in
-    let r = ref (i + 1) in
-    let smudge_count = ref 0 in
-    while !l >= 0 && !r < Array.length grid do
-      smudge_count := !smudge_count + (count_difference grid.(!l) grid.(!r));
-      l := !l - 1;
-      r := !r + 1;
-    done;
-    if !smudge_count = 1 then
-      result := Some (i + 1)
-  done;
-  !result
+let rec find_exact_symmetry i grid =
+  if i = Array.length grid then
+    None
+  else if test_exact_symmetry grid (pred i) i then
+    Some i
+  else
+    find_exact_symmetry (succ i) grid
+
+let rec test_smudged_symmetry grid l r accum =
+  if l < 0 || r >= Array.length grid then
+    accum = 1
+  else
+    let accum = count_difference grid.(l) grid.(r) 0 accum in
+    if accum > 1 then
+      false
+    else
+      test_smudged_symmetry grid (pred l) (succ r) accum
+
+let rec find_smudged_symmetry i grid =
+  if i = Array.length grid then
+    None
+  else if test_smudged_symmetry grid (pred i) i 0 then
+    Some i
+  else
+    find_smudged_symmetry (succ i) grid
 
 let transpose_grid grid =
   let w = Array.length grid in
@@ -57,27 +60,29 @@ let transpose_grid grid =
     Array.init w make_col in
   Array.init h make_row
 
-let find_symmetry grid =
-  match test_horizontal_symmetry grid with
-  | Some value -> (100 * value)
-  | None ->
-    match test_horizontal_symmetry (transpose_grid grid) with
-    | Some value -> value
-    | None -> failwith ""
+let rec calculate f grids accum =
+  match grids with
+  | g :: gs ->
+    let v = match f g with
+    | Some value -> (100 * value)
+    | None ->
+      match f (transpose_grid g) with
+      | Some value -> value
+      | None -> failwith "" in
+    calculate f gs (v + accum)
+  | [] -> accum
 
-let fix_smudge grid =
-  match test_smudged_horizontal_symmetry grid with
-  | Some value -> (100 * value)
-  | None ->
-    match test_smudged_horizontal_symmetry (transpose_grid grid) with
-    | Some value -> value
-    | None -> failwith ""
+let part_1 grids = 
+  print_int (calculate (find_exact_symmetry 1) grids 0);
+  print_endline ""
+
+let part_2 grids =
+  print_int (calculate (find_smudged_symmetry 1) grids 0);
+  print_endline ""
 
 let () =
   let ic = open_in "input" in
   let grids = read_grids ic [] [] in
-  print_int (List.fold_left (+) 0 (List.map find_symmetry grids));
-  print_endline "";
-  print_int (List.fold_left (+) 0 (List.map fix_smudge grids));
-  print_endline "";
-  ()
+  close_in ic;
+  part_1 grids;
+  part_2 grids
